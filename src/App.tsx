366e2f3bbd06067dc7e14e4ee1bdec5ae806b9c5
Wang Tiaoke <100271394+wngtk@users.noreply.github.com>
2024-06-05 21:17:35 +0800

    Exercise 9.17

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react'
-import { NonSensitiveDiaryEntry } from './types'
+import { SyntheticEvent, useEffect, useState } from 'react'
+import { NonSensitiveDiaryEntry, Visibility, Weather } from './types'
 import diaryService from './services/diaryService'
 import "./index.css"
 
@@ -11,8 +11,47 @@ function App() {
       .then(data => setDiaries(data))
   }, [])
 
+  const submit = async (event: SyntheticEvent) => {
+    event.preventDefault()
+
+    const target = event.target as typeof event.target & {
+      date: { value: string },
+      visibility: { value: Visibility },
+      weather: { value: Weather },
+      comment: { value: string }
+    }
+
+    const newDiary = await diaryService.addDiary({
+      date: target.date.value,
+      comment: target.comment.value,
+      visibility: target.visibility.value,
+      weather: target.weather.value
+    })
+    setDiaries(diaries.concat(newDiary))
+  }
+
   return (
     <>
+      <h2>Add new entry</h2>
+      <form onSubmit={submit}>
+        <div>
+          date
+          <input type="text" name='date' />
+        </div>
+        <div>
+          visibility
+          <input type="text" name='visibility' />
+        </div>
+        <div>
+          weather
+          <input type="text" name='weather' />
+        </div>
+        <div>
+          comment
+          <input type="text" name='comment' />
+        </div>
+        <button>add</button>
+      </form>
       <h2>Diary entries</h2>
       {diaries
         .map(d =>
--- src/services/diaryService.ts
+++ src/services/diaryService.ts
@@ -1,11 +1,17 @@
 import axios from "axios"
-import { NonSensitiveDiaryEntry } from "../types"
+import { DiaryEntry, NewDiaryEntry, NonSensitiveDiaryEntry } from "../types"
 
 const getAllDiaries = async () => {
   const response = await axios.get('/api/diaries')
   return response.data as NonSensitiveDiaryEntry[]
 }
 
+const addDiary = async (object: NewDiaryEntry) => {
+  const response = await axios.post('/api/diaries', object)
+  return response.data as DiaryEntry
+}
+
 export default {
-  getAllDiaries
+  getAllDiaries,
+  addDiary
 }
